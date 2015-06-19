4c691c9402803bd7122281073feca1fbe580ab0b
leebj91 <leebj91@naver.com>
2015-06-19 12:25:29 +0900

    hhh

--- project/src/main/java/dao/MemberDao.java
+++ project/src/main/java/dao/MemberDao.java
@@ -3,10 +3,8 @@ package dao;
 import model.Member;
 
 public interface MemberDao {
-<<<<<<< HEAD
+
 	public String getId(String mid);
 
-=======
->>>>>>> b2315bba455bfbda78f41d5137e997f41d4e7d5a
 
 }
--- project/src/main/java/dao/MemberDaoImpl.java
+++ project/src/main/java/dao/MemberDaoImpl.java
@@ -1,6 +1,6 @@
 package dao;
 
-<<<<<<< HEAD
+
 import java.io.IOException;
 import java.io.Reader;
 
@@ -9,10 +9,7 @@ import java.io.Reader;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-=======
-import model.Member;
 
->>>>>>> b2315bba455bfbda78f41d5137e997f41d4e7d5a
 import org.springframework.stereotype.Repository;
 
 import com.ibatis.common.resources.Resources;
@@ -29,7 +26,7 @@ public class MemberDaoImpl implements MemberDao{
 		return qsession;
 	}
 
-<<<<<<< HEAD
+
 	public String getId(String id) {
 		String id2 = null;
 		SqlSession qsession = null;
@@ -43,13 +40,5 @@ public class MemberDaoImpl implements MemberDao{
 		}
 		return id2;
 	}
-	
-=======
-	@Override
-	public Member findByUserIdAndPassword(String userId, String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
->>>>>>> b2315bba455bfbda78f41d5137e997f41d4e7d5a
 }
